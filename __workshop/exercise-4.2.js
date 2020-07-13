37fd99e24f206ac6e9b792825fc4e6f8917cf4fe
johnnytortorici <johnny.tortorici@gmail.com>
2020-07-13 19:17:50 -0400

    Exercise 4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,16 @@
 const request = require('request-promise');
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  try {
+    const response = await request({
+      uri: 'https://api.tronalddump.io/random/quote',
+      headers: { 'Accept': 'application/json' }
+    });
+    const quote = JSON.parse(response).value;
+    console.log(quote);
+  } catch (err) {
+    console.log('ERROR: ', err);
+  }
 };
+
+getTronaldDumpQuote();
