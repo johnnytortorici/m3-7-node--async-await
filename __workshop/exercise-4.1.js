ea65e50689d3f54aa9da4ce01c10202c9c0f1f13
johnnytortorici <johnny.tortorici@gmail.com>
2020-07-13 17:57:51 -0400

    Exercise 4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,27 @@
 const request = require('request-promise');
 
 // getDadJoke
+const getDadJoke = () => {
+    const joke = new Promise((resolve, reject) => {
+        const response = request({
+            uri: 'https://icanhazdadjoke.com/',
+            headers: { 'Accept': 'application/json' }
+        })
+            .then((result) => JSON.parse(result))
+            .then((result) => result.joke);
+        resolve(response);
+    })
+    return joke;
+};
+
+const getJoke = async () => {
+    try {
+        const joke = await getDadJoke();
+        console.log(joke);
+    } catch(err) {
+        console.log('ERROR: ', err)
+    }
+}
 
 // 4.1
-// getJoke().then((data) => console.log(data));
+getJoke();
