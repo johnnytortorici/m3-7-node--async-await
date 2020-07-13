7adce261b70d8491e78387a06e209b81f9fb9049
johnnytortorici <johnny.tortorici@gmail.com>
2020-07-13 19:37:13 -0400

    Exercise 4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,13 @@
 const request = require('request-promise');
 
 const getGeekJoke = async () => {
-  // ha hahahh
+  try {
+    const response = await request('https://geek-jokes.sameerkumar.website/api?format=json');
+    const joke = JSON.parse(response).joke;
+    console.log(joke);
+  } catch (err) {
+    console.log('ERROR: ', err);
+  }
 };
+
+getGeekJoke();
